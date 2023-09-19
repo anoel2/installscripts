84f99a45530ac135513e5c4fec6955fc1c62e7ef
Alex Noel <116229513+anoel2@users.noreply.github.com>
2023-09-19 10:38:22 -0600

    Create rhel8_agent.bash

---
+++ puppet/rhel8_agent.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Check if script is being run as root
+if [ $(id -u) -ne 0 ]; then
+    echo "This script must be run as root"
+    exit 1
+fi
+
+puppetservername=""
+puppetserverip=""
+
+
+rpm -Uvh https://yum.puppet.com/puppet7-release-el-8.noarch.rpm
+yum install puppet-agent -y
+source /etc/profile.d/puppet-agent.sh
+puppet config set server $puppetserver --section main
+puppet ssl bootstrap
+echo "$puppetserverip $puppetservername">> /etc/hosts
+systemctl start puppet
+systemctl enable puppet
