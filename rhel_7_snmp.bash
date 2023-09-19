99398801a63a9c970e15d37f0dab1a59b3ab9f65
Alex Noel <116229513+anoel2@users.noreply.github.com>
2023-09-19 10:26:42 -0600

    Update rhel_7_snmp.bash

--- rhel_7_snmp.bash
+++ rhel_7_snmp.bash
@@ -1,1 +1,16 @@
-#
+#!/bin/bash
+
+authpass=""
+cryptopass=""
+authalgo="SHA"
+cryptoalgo="AES"
+snmpuser=""
+
+yum install net-snmp net-snmp-utils net-snmp-devel -y
+systemctl stop snmpd
+net-snmp-config --create-snmpv3-user -A $authpass -X $cryptopass -a $authalgo -x $cryptoalgo $snmpuser
+firewall-cmd --permanent --add-port=161/udp
+yum firewall-cmd --add-port=161/udp
+firewall-cmd --reload
+systemctl enable snmpd
+systemctl start snmpd
