92bd5a2064903ab4eb34545a4a6ec760f859aad8
Alex Noel <116229513+anoel2@users.noreply.github.com>
2023-09-25 09:19:10 -0600

    Update rhel_7.bash
    removed yum from line 17

--- snmp/rhel_7.bash
+++ snmp/rhel_7.bash
@@ -14,7 +14,7 @@ yum install net-snmp net-snmp-utils net-snmp-devel -y
 systemctl stop snmpd
 net-snmp-config --create-snmpv3-user -A $authpass -X $cryptopass -a $authalgo -x $cryptoalgo $snmpuser
 firewall-cmd --permanent --add-port=161/udp
-yum firewall-cmd --add-port=161/udp
+firewall-cmd --add-port=161/udp
 firewall-cmd --reload
 systemctl enable snmpd
 systemctl start snmpd
